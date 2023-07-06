8f75cb6034bc12e66e12274ae6d1d6eb9f5f23d5
Violet Safarova <keomanivongvii@yandex.com>
2023-07-06 15:28:43 +0500

    add changes/trying to find key

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -4,13 +4,12 @@ import { useState } from 'react';
 
 const Users = () => { 
   const [users, setUsers] = useState(api.users.fetchAll());
-  const [count, setCount] = useState(api.users.fetchAll().length);
 
   const createHeader = () => {
-   return count <= 12 && count !== 0 ?
+   return users.length <= 12 && users.length !== 0 ?
     (
       <>
-      <span className = {getBageClasses()}> {count} человек тусанет с тобой сегодня</span> 
+      <span className = {getBageClasses()}> {users.length} человек тусанет с тобой сегодня</span> 
       </>
     )
      : 
@@ -23,65 +22,53 @@ const Users = () => {
 
   const getBageClasses = () => {
     let classes = 'badge  m-2 '
-    classes += count === 0 ? "bg-danger"  : "bg-primary";
+    classes += users.length === 0 ? "bg-danger"  : "bg-primary";
     return classes;
   };
 
   const handleDecrement = (userId) => {
-    setCount((prevState)=> prevState - 1)
     setUsers(prevState => prevState.filter(user => user !== userId))
   };
 
-  const createTHead = () => {
-   return count <= 12 && count !== 0 ? 
-   (
-    <>
-    <thead>
-    <tr>
+  function header() {
+    return (
+      users.length <= 12 && users.length !== 0 ? 
+      <tr key={0}>
       <th scope="col">Имя</th>
       <th scope="col">Качества</th>
       <th scope="col">Профессия</th>
       <th scope="col">Встретился, раз</th>
       <th scope="col">Оценка</th>
       <th scope="col"></th>
-    </tr>
-   </thead>
-   </>
-   ) 
-   :
-   (
-    <>
-    <thead hidden></thead>
-    </>
-   )
-}
-
+      </tr>
+     :
+      <tr hidden></tr>
+    )
+  }
 return  ( <>
-  <h2>{createHeader(count)}</h2>
-  <table className = "table table table-sm">
-      {createTHead()}
+
+  <h2>{createHeader(users.length)}</h2>
+  <table className = "table table-success table-striped"> {/*table table table-sm */}
+  <thead>{header()}</thead>
+  
 <tbody>
-  {
-  users.map(user=> { 
-  return ( <>
-   <tr >
-    <td key = {user.name}>{user.name}</td>
-    <td>{
-     user.qualities.map(quality => {
-     const qualityColor = quality.color;
-     let classes = 'm-1 badge bg-';
-     classes+=qualityColor;
-     return <tr key = {quality._id} className={classes}>{quality.name}</tr>})
-    }</td>
-    <td key = {user.profession._id}>{user.profession.name}</td>
-    <td key ={user.completedMeetings} >{user.completedMeetings}</td>
-    <td key = {user.rate}>{user.rate}/5</td>
-    <td id = {user._id}><button onClick={() => handleDecrement(user)} className ="btn btn-danger">Delete</button></td>
-   </tr>
-   </> ) }
+  { users.map(user=> { 
+  return ( 
+  <tr key={user._id}>
+   <td >{user.name}</td>
+   <td >{ user.qualities.map(quality => {
+     return <tr key={quality._id} className={`m-1 badge bg-${quality.color}`}>{quality.name}</tr>})
+     } </td>
+   <td>{user.profession.name}</td>
+   <td>{user.completedMeetings}</td>
+   <td>{user.rate}/5</td>
+   <td><button onClick={() => handleDecrement(user)} className ="btn btn-danger">Delete</button></td>
+  </tr>
   )}
+ )}
 </tbody>
 </table>
 </> )
+
 }
 export default Users;
